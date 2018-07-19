5992fc09ddefec92ac78a274eb352d877f544cdd
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2018-07-19 17:07:26 +0200

    Aggiunto script per creare il file zip del report (non funzionante)

---
+++ crea_zip.ps1
@@ -0,0 +1,12 @@
+﻿
+$AppProps = convertfrom-stringdata (get-content ./ReportVersion.properties -raw)
+$currentPath = $((get-Item -Path ".\").FullName)
+$outputFileName = $currentPath + '\' + $AppProps.ReportN + " " + $appProps.ReportV + '.zip'
+
+Write-Host $currentPath
+Write-Host $outputFileName
+
+$quotedPath = '"' + $currentPath + '"'
+
+Add-Type -Assembly System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::CreateFromDirectory($currentPath, $outputFileName)
