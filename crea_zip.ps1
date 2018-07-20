205eef2ff2b15628b0cfa2c5e9f5e75add00616c
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2018-07-20 08:25:23 +0200

    Modificato il funzionamento: Copia i file del report in una cartella temporanea, crea uno zip, poi lo sposta nella cartella di destinazione. Funziona

--- crea_zip.ps1
+++ crea_zip.ps1
@@ -3,15 +3,24 @@ $AppProps = convertfrom-stringdata (get-content ./ReportVersion.properties -raw)
 $currentPath = $((get-Item -Path ".\").FullName)
 $outputFileName = $currentPath + '\' + $AppProps.ReportN + " " + $appProps.ReportV + '.zip'
 
-Write-Host $currentPath
-Write-Host $outputFileName
-
 $quotedPath = '"' + $currentPath + '"'
 
+
+# Crea una cartella temporanea
+$parent = [System.IO.Path]::GetTempPath()
+$name = [System.IO.Path]::GetRandomFileName()
+$tempDirectory = New-Item -ItemType Directory -Path (Join-Path $parent $name)
+
+# Copia i file da mettere nello zip nella cartella temporanea
+Get-ChildItem $currentPath -Include "*.jasper", "*.jrxml", "*.properties"  -Recurse | Select -ExpandProperty FullName | Copy-Item -Destination $tempDirectory
+
+# Crea zip con il contenuto della cartella (in una cartella temporanea)
 $tempFile = [System.IO.Path]::GetTempPath() + [System.IO.Path]::GetRandomFileName()
-
 Add-Type -Assembly System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::CreateFromDirectory($tempDirectory, $tempFile)
 
-[System.IO.Compression.ZipFile]::CreateFromDirectory($currentPath, $tempFile)
-
+# Sposta il file zip nella cartella corrente
 Move-Item $tempFile $outputFileName
+
+# Cancella la cartella temporanea
+Remove-Item -Recurse $tempDirectory
