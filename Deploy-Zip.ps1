9c94272ad454c01e5075a942f59d30f156c43e2a
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2018-07-25 22:38:01 +0200

    Aggiunto #Requires Aggiunto parametro opzionale deployPath per specificare la directory dove verrà creato il file zip Aggiunto output che mostra se e dove è stato creato il file Corretto messaggio di errore

--- Deploy-Zip.ps1
+++ Deploy-Zip.ps1
@@ -1,17 +1,23 @@
-﻿# Recupera Nome report e versione dal file Report.properties
+﻿#Requires -Version 3
+
+Param(
+  [Parameter(Mandatory=$false)][string]$deployPath = ".\"
+)
+
+# Recupera Nome report e versione dal file Report.properties
 $AppProps = convertfrom-stringdata (get-content ./Report.properties -raw)
 if ([string]::IsNullOrEmpty($AppProps.ReportName) -or [string]::IsNullOrEmpty($AppProps.ReportVersion)) {
-    Write-Host "File ./Report.properties mancante."
+    Write-Host "File ./Report.properties mancante o non valido."
     exit(-1)
 }
-$currentPath = $((get-Item -Path ".\").FullName)
-$outputFileName = $currentPath + '\' + $AppProps.ReportName + " " + $appProps.ReportVersion + '.zip'
+$outputPath = $((get-Item -Path $deployPath).FullName)
+$zipName = $AppProps.ReportName + " " + $appProps.ReportVersion + '.zip'
+$outputFileName = $outputPath + '\' + $zipName
 
 # Crea una cartella temporanea
 $parent = [System.IO.Path]::GetTempPath()
 $name = [System.IO.Path]::GetRandomFileName()
-$tempDirectory = New-Item -ItemType Directory -Path (Join-Path $parent $name)
-
+$tempDirectory = New-Item -ItemType Directory -Path (Join-Path $parent $name) -ErrorAction stop
 # Copia i file da mettere nello zip nella cartella temporanea
 Get-ChildItem $currentPath -Include "*.jasper", "*.jrxml", "*.properties" -Recurse | Select -ExpandProperty FullName | Copy-Item -Destination $tempDirectory
 
@@ -21,7 +27,9 @@ Add-Type -Assembly System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::CreateFromDirectory($tempDirectory, $tempFile)
 
 # Sposta il file zip nella cartella corrente
-Move-Item $tempFile $outputFileName
+Move-Item $tempFile $outputFileName -ErrorAction stop
+
+write-host $zipName" creato in "$outputPath
 
 # Cancella la cartella temporanea
 Remove-Item -Recurse $tempDirectory
