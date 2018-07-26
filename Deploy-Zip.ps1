12205644107cc8abe4c26bbdd182fc69cc96470e
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2018-07-26 08:54:52 +0200

    Riorganizzato codice, aggiunto blocco try/catch. Cambiato nomi variabili Usato join-path ove possibile Aggiunto campo .NOTES con versione

--- Deploy-Zip.ps1
+++ Deploy-Zip.ps1
@@ -1,35 +1,46 @@
 ﻿#Requires -Version 3
 
+<#
+.NOTES
+    Version: 1.1.0
+#>
+
 Param(
-  [Parameter(Mandatory=$false)][string]$deployPath = ".\"
+  [Parameter(Mandatory=$false)][string]$DeployPath = ".\"
 )
 
 # Recupera Nome report e versione dal file Report.properties
-$AppProps = convertfrom-stringdata (get-content ./Report.properties -raw)
+$AppProps = convertfrom-stringdata (get-content ./Report.properties -raw -ErrorAction SilentlyContinue)
 if ([string]::IsNullOrEmpty($AppProps.ReportName) -or [string]::IsNullOrEmpty($AppProps.ReportVersion)) {
     Write-Host "File ./Report.properties mancante o non valido."
     exit(-1)
 }
-$outputPath = $((get-Item -Path $deployPath).FullName)
+
+$fullDeployPath = $((get-Item -Path $DeployPath).FullName)
 $zipName = $AppProps.ReportName + " " + $appProps.ReportVersion + '.zip'
-$outputFileName = $outputPath + '\' + $zipName
+$fullDeployFileName = join-Path $fullDeployPath $zipName
 
 # Crea una cartella temporanea
 $parent = [System.IO.Path]::GetTempPath()
 $name = [System.IO.Path]::GetRandomFileName()
 $tempDirectory = New-Item -ItemType Directory -Path (Join-Path $parent $name) -ErrorAction stop
-# Copia i file da mettere nello zip nella cartella temporanea
-Get-ChildItem $currentPath -Include "*.jasper", "*.jrxml", "*.properties" -Recurse | Select -ExpandProperty FullName | Copy-Item -Destination $tempDirectory
+try {
+    # Copia i file da mettere nello zip nella cartella temporanea
+    $currentPath = $((get-Item -Path '.\').FullName)
+    Get-ChildItem $currentPath -Include "*.jasper", "*.jrxml", "*.properties" -Recurse | Select -ExpandProperty FullName | Copy-Item -Destination $tempDirectory
 
-# Crea zip con il contenuto della cartella (in una cartella temporanea)
-$tempFile = [System.IO.Path]::GetTempPath() + [System.IO.Path]::GetRandomFileName()
-Add-Type -Assembly System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::CreateFromDirectory($tempDirectory, $tempFile)
+    # Crea file zip temporaneo in una cartella temporanea con il contenuto della cartella creata prima
+    $tempFile = [System.IO.Path]::GetTempPath() + [System.IO.Path]::GetRandomFileName()
+    Add-Type -Assembly System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($tempDirectory, $tempFile)
 
-# Sposta il file zip nella cartella corrente
-Move-Item $tempFile $outputFileName -ErrorAction stop
-
-write-host $zipName" creato in "$outputPath
+    # Sposta il file zip nella cartella corrente
+    Move-Item $tempFile $fullDeployFileName -ErrorAction stop
+    write-host $zipName" creato in "$fullDeployPath
+} catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host -ForegroundColor Red 'Errore:' $ErrorMessage
+}
 
 # Cancella la cartella temporanea
 Remove-Item -Recurse $tempDirectory
