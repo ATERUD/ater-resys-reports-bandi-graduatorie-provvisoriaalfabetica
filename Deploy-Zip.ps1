d29ab6b52c9fe7bd91bff3de1fd4f022a4dfb9f2
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2018-07-26 11:27:39 +0200

    Usa la directory corrente anche per recuperare il file Report.Properties. Commentata la parte con il parametro perchè con PowerShell 3.0, se lanciato da explorer, lo script non funziona. Bug noto.

--- Deploy-Zip.ps1
+++ Deploy-Zip.ps1
@@ -1,23 +1,35 @@
 ﻿#Requires -Version 3
 
 <#
+.SYNOPSIS
+    Crea un file ZIP per il deploy del report
+.DESCRIPTION
+    Crea un file ZIP per il deploy del report
+.PARAMETER $DeployPath
+    Percorso dove creare il file Zip contenente il report
 .NOTES
     Version: 1.1.0
+.EXAMPLE
+    Deploy-Zip.ps1 -DeployPath c:\temp 
 #>
 
-Param(
-  [Parameter(Mandatory=$false)][string]$DeployPath = ".\"
-)
+# A causa di un bug nell'installazione di PowerShell 3.0, lo script non funziona quando lanciato da explorer
+
+#param([Parameter(Mandatory=$false)][string]$DeployPath = '.\') 
+
+$DeployPath = '.\'
+
+$currentPath = $((get-Item -Path '.\').FullName)
 
 # Recupera Nome report e versione dal file Report.properties
-$AppProps = convertfrom-stringdata (get-content ./Report.properties -raw -ErrorAction SilentlyContinue)
+$AppProps = convertfrom-stringdata (get-content $(join-path $currentPath 'Report.Properties') -raw)
 if ([string]::IsNullOrEmpty($AppProps.ReportName) -or [string]::IsNullOrEmpty($AppProps.ReportVersion)) {
     Write-Host "File ./Report.properties mancante o non valido."
     exit(-1)
 }
 
 $fullDeployPath = $((get-Item -Path $DeployPath).FullName)
-$zipName = $AppProps.ReportName + " " + $appProps.ReportVersion + '.zip'
+$zipName = $AppProps.ReportName + ' ' + $appProps.ReportVersion + '.zip'
 $fullDeployFileName = join-Path $fullDeployPath $zipName
 
 # Crea una cartella temporanea
@@ -26,7 +38,6 @@ $name = [System.IO.Path]::GetRandomFileName()
 $tempDirectory = New-Item -ItemType Directory -Path (Join-Path $parent $name) -ErrorAction stop
 try {
     # Copia i file da mettere nello zip nella cartella temporanea
-    $currentPath = $((get-Item -Path '.\').FullName)
     Get-ChildItem $currentPath -Include "*.jasper", "*.jrxml", "*.properties" -Recurse | Select -ExpandProperty FullName | Copy-Item -Destination $tempDirectory
 
     # Crea file zip temporaneo in una cartella temporanea con il contenuto della cartella creata prima
